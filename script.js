de73099ac99f1a155c6fc81b3501490ac9728be9
David Bowen <dsbmusic328@gmail.com>
2017-12-05 18:23:32 +0100

    Added dark CSS file and JS file to change between CSS files based on time of day. Removed unused CSS references in header of index.html

---
+++ script.js
@@ -0,0 +1,28 @@
+(function(){
+  'use strict'
+
+  document.addEventListener('DOMContentLoaded', function(){
+
+
+    setTimeout(stylesheetChange, 1);
+    setInterval(stylesheetChange, 3600000);
+
+    function stylesheetChange() {
+      var d = new Date(),
+        hours = d.getHours(),
+        // hours = 14,
+        csslight = document.getElementById('css_light'),
+        cssdark = document.getElementById('css_dark');
+
+      if (hours < 7 || hours >= 19) {
+        csslight.disabled = true;
+        cssdark.disabled = false;
+      } else {
+        csslight.disabled = false;
+        cssdark.disabled = true;
+      }
+    }
+
+  });
+
+})();
